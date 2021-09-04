ef98a629a05b3e4d88fd5d88bc4b0cc58c8ee243
mh8 <moazzemhb@gmail.com>
2021-09-05 00:33:10 +0600

    Change

--- partials/_sidebar.php
+++ partials/_sidebar.php
@@ -33,7 +33,7 @@
   <aside class="main-sidebar elevation-4 sidebar-light-teal">
     <!-- Brand Logo -->
     <a href="index.php" class="brand-link navbar-teal">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <!-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> -->
       <span class="brand-text font-weight-light fs-3">R M S</span>
     </a>
 
@@ -42,10 +42,10 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <!-- <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image"> -->
         </div>
         <div class="info">
-          <a href="#" class="d-block fs-5">Abu Sayed Rana</a>
+          <a href="#" class="d-block fs-5">Admin</a>
         </div>
       </div>
 
